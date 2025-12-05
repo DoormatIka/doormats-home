44d5d9343693009fcc8a656b8ed19d06abe9ffe1
DoormatIka <yomotouhou@gmail.com>
2025-12-05 10:50:26 +0800

    implementing own hash router

--- room/room.js
+++ room/room.js
@@ -1,14 +1,53 @@
 
-import { HashRoute } from "vanilla-routing";
-
 const room = "/room.html"
+// derived from https://github.com/frentsel/eRouter/blob/master/eRouter.js
+// thank you!
+class HashRouter {
+	constructor() {
+		this.hash = "#/";
+		this.routes = {};
+	}
+	add(route, fn) {
+		this.routes[route] = fn;
+	}
+	onHashChange() {
+		window.addEventListener("hashchange", this.onHashChangeInternal, false);
+	}
+	onHashChangeInternal() {
+		// i need to handle normal paths with hash paths too.
+		// e.g: localhost:8000/normalpath/#/hashroutetoo
+		let uri = window.location.hash;
+		let params;
+
+		if (uri.indexOf(this.hash) === -1)
+			return window.location.hash = this.hash;
+
+		uri = uri.split(hash).pop();
+
+		if (!uri.length) return this.routes.index();
+
+		if (uri.indexOf('/') > -1) {
+			params = uri.split('/');
+			uri = params.shift();
+		}
+
+		if (!this.routes[uri])
+			return this.routes.notFound(uri);
+		this.routes[uri].apply(this, params);
+	}
+	set(path) {
+		// get previous path.
+		window.location.hash = this.hash + path;
+	}
+}
 // read file from a .html file.
 // use a loading screen when the page is still loading!
+/*
 document.addEventListener("DOMContentLoaded", () => {
 	console.log("loaded router");
 	HashRoute([
 		{ 
-			pathname: "home/",
+			pathname: "home",
 			element: () => {
 				console.log("loading path / into router")
 				const container = document.createElement("div");
@@ -37,6 +76,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		}
 	])
 });
+*/
 
 function makeLoadingDiv() {
 	const d = document.createElement("div");
