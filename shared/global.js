caf0241cd69f0e5f177ac7611cc3e30b5d96dbe6
DoormatIka <yomotouhou@gmail.com>
2025-12-13 00:39:20 +0800

    scroll thumb position

--- shared/global.js
+++ shared/global.js
@@ -7,33 +7,56 @@ import { CursorManager } from "/shared/components/cursor";
 
 const man = new CursorManager();
 
-/**
-	* @type {HTMLDivElement | undefined}
-	*/
-let scrollThumb = undefined;
+const elements = {
+	thumb: null,
+	shell: null,
+	container: null,
+	line: null,
+}
+
+function positionScrollbar() {
+	const thumb = document.getElementById("scrollbar-thumb");
+	const shell = document.querySelector(".shell");
+	const container = document.querySelector(".main-container");
+	const line = document.querySelector("#scrollbar");
+
+	const lineRect = line.getBoundingClientRect();
+	const thumbStyle = window.getComputedStyle(thumb);
+	const h_center = (parseFloat(thumbStyle.height) / 2) || 0;
+	const w_center = (parseFloat(thumbStyle.width) / 2) || 0;
+	console.log(thumbStyle.height, thumbStyle.width, lineRect.top, lineRect.left, thumbStyle.position)
+	// WHY IS thumb.style NOT DOING ANYTHINGGGG OMSJKDFBAKJS
+	const absoluteTop = lineRect.top - h_center;
+	const absoluteLeft = lineRect.left - (w_center + 2.5);
+	thumb.style.top = `${absoluteTop}px`;
+	thumb.style.left = `${absoluteLeft}px`;
+}
 
 document.addEventListener("DOMContentLoaded", () => {
 	document.fonts.ready.then(() => {
 		document.documentElement.style.visibility = "visible";
 
+		elements.thumb = document.getElementById("scrollbar-thumb");
+		const shell = document.querySelector(".shell");
+		const container = document.querySelector(".main-container");
+		const line = document.querySelector("#scrollbar");
+
 		man.initCursor();
 		requestAnimationFrame(man.animateCursor);
 		man.enableMouseMovement();
 
-		/**
-			* @type {any}
-			*/
-		const bar = document.getElementById("scrollbar-thumb");
-		scrollThumb = bar;
+		positionScrollbar();
 	})
 })
 
-/*
+window.addEventListener("resize", () => {
+	positionScrollbar();
+});
 
+/*
 	MAKE A CUSTOM SCROLLBAR!!
-	- how to position scroll thumb to the vertical divider no matter what size it is.
+	- [/] how to position scroll thumb to the vertical divider no matter what size it is.
 	- how to make that move up and down on the divider.
 	- making it longer and shorter depending on the scroll size.
-
 */
 
