90f5535bdcefe9748a0b030b7f6db8e93584fb6e
DoormatIka <yomotouhou@gmail.com>
2025-12-17 01:05:51 +0800

    todo: manifest file per route

--- index.js
+++ index.js
@@ -5,6 +5,20 @@
 import { HashRouter } from "/shared/components/router.js";
 import { resizeScrollbar, positionScrollbar } from "/shared/components/scrollbar";
 
+
+/**
+	* Very hacky fix to detect if a URL is a valid HTML file.
+	*
+	* I will fix this by adding a build step.
+	* It should generate a manifest file from the /pages directory,
+	* 	that contains every possible path with an `.html` file.
+	* @param {string} url 
+	* @returns {Promise<boolean>}
+	*/
+function isHTMLFileValid(url) {
+	return new Promise(res => {});
+}
+
 /**
 	* Loads file into text.
 	* @param {string} path 
@@ -13,6 +27,7 @@ export function loadFile(path) {
 	return new Promise((res, rej) => {
 		fetch(path)
 			.then(resp => {
+				console.log(resp);
 				if (!resp.ok) {
 					rej(`Cannot load HTML file: "${path}"!`)
 				}
