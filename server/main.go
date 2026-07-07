17edcda1e7ab9319cd9cb54768bef1c90871c025
DoormatIka <yomotouhou@gmail.com>
2026-07-07 22:30:43 +0800

    feat/server: cors headers

--- server/main.go
+++ server/main.go
@@ -1,22 +1,37 @@
-
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"server/handlers"
 	"server/repository"
 	"server/services"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 	router.SetTrustedProxies([]string{"127.0.0.1", "::1"})
 
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"http://127.0.0.1:8080",
+			"http://localhost:8080",
+			"https://home.yuyuqk.com",
+		},
+		AllowMethods: []string{"GET", "POST"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders: []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge: 4 * time.Hour,
+	}))
+
 	repo := &repository.PingRepository{}
 	service := &services.PingService{Repo: repo}
 	handler := &handlers.PingHandler{Service: service}
 
-	router.GET("/ping/", handler.GetGarbageData)
+	router.GET("/ping", handler.GetGarbageData)
 
 	router.Run(":8980")
 }
