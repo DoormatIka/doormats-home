76e8ccf1399eaa2fc5affd126d6353f30108f9b2
DoormatIka <yomotouhou@gmail.com>
2026-07-08 15:31:18 +0800

    feat/server: added rate-limiting

--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/http"
 	"server/handlers"
 	"server/repository"
 	"server/services"
@@ -8,12 +9,29 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/time/rate"
 )
 
+func RateLimiter() gin.HandlerFunc {
+	limiter := rate.NewLimiter(1, 4)
+	return func(ctx *gin.Context) {
+		if limiter.Allow() {
+			ctx.Next()
+		} else {
+			ctx.JSON(http.StatusTooManyRequests, gin.H{
+				"message": "Limit exceeded!",
+			})
+		}
+	}
+}
+
+
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 	router.SetTrustedProxies([]string{"127.0.0.1", "::1"})
 
+	router.Use(RateLimiter())
 	router.Use(cors.New(cors.Config{
 		AllowOrigins: []string{
 			"http://127.0.0.1:8080",
